d3ec1dcde6a1e4d3718341d2f011ffb8183d79d6
DeadWalrus <pointlesstack@gmail.com>
2020-05-10 16:59:59 -0400

    Have to work on this on other comp

--- print_int.asm
+++ print_int.asm
@@ -1,1 +1,10 @@
 ;; Print a signed integer in decimal format ;;
+
+print_int:
+    ;mov rax, rdi    ;signed number stored in rdi
+    cmp rdi, 0
+    jge .printpos
+    
+
+    .printpos:
+        %include "print_uint1.asm"
--- print_uint1.asm
+++ print_uint1.asm
@@ -1,4 +1,3 @@
-%include "print_string.asm"
 ;; Print an unsigned integer to stdout ;;
 print_uint:
     mov rax, rdi                ;argument passed in rdi
@@ -7,18 +6,17 @@ print_uint:
     mov rdi, 10                 ;mov 10 into rdi to isolate valued in rax
 
     .loop:
-        test rax, rax           ;if rax stores 0
-        jz .break               ;break out of loop
         div rdi                 ;isolate last digit in rax
         dec rsp                 ;allocate stack space for digit
         add dl, 0x30            ;transform to ASCII code
         mov byte[rsp], dl       ;move character to stack
         mov rdx, 0              ;zero rdx, was *fixed floating point exception error
-        jmp .loop               ;jump to loop
-
-    .break:
+        test rax, rax
+        jnz .loop               ;jump to loop if rax != 0
+        
         mov rdi, rsp            ;print_string takes address to string in rdi
         call print_string       ;print the string stored on stack
         inc rax                 ;inc rax
         add rsp, rax            ;deallocate space on stack using string length
         ret
+%include "print_string.asm"
