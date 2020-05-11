44339491b51d41569d2ca31c0400f700c03c2983
DeadWalrus <pointlesstack@gmail.com>
2020-05-10 22:50:23 -0400

    print_int now prints signed integers instead of garbage

--- print_int.asm
+++ print_int.asm
@@ -1,10 +1,14 @@
 ;; Print a signed integer in decimal format ;;
-
+%include "print_char.asm"
 print_int:
-    ;mov rax, rdi    ;signed number stored in rdi
-    cmp rdi, 0
-    jge .printpos
-    
-
+    ;mov rax, rdi       ;signed number stored in rdi
+    cmp rdi, 0          ;see if number is negative
+    jge .printpos       
+    push rdi            ;print_char changes rdi
+    mov rbx, '-'        ;negative character
+    call print_char
+    pop rdi
+    neg rdi             ;negate(make positive) rdi
+    xor rdx, rdx        ;clear rdx
     .printpos:
         %include "print_uint1.asm"
