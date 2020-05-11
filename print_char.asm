0a9adaea9ac047e8af70a81cc2bfda6a250c92dd
DeadWalrus <pointlesstack@gmail.com>
2020-05-10 22:49:02 -0400

    print_char now takes character in rbx

--- print_char.asm
+++ print_char.asm
@@ -1,10 +1,13 @@
 ;char: db 0
 print_char:
+    dec rsp
+    mov byte[rsp], bl
     mov rax, 1
+    mov rsi, rsp
     mov rdx, 1
-    lea rsi, [rsp+8]
     mov rdi, 1
     push rcx
     syscall 
     pop rcx
+    inc rsp
     ret
